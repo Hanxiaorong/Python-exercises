3cbac09745b42daa40252ac95dbb7157f0d9b56e
Hanxiaorong <30439925+Hanxiaorong@users.noreply.github.com>
2017-08-01 11:18:27 +0800

    习题12

---
+++ ex12-1.py
@@ -0,0 +1,11 @@
+﻿age = raw_input("How old are you?")  
+height = raw_input("How tall are you ?")
+weight = raw_input("How much do you weigh?")
+
+print "So, you're %r old, %r tall, %r heavy." %(
+   age, height, weight)
+   
+   
+   
+# 学习使用pydoc, pydoc 好像是对一个函数的解释
+# >python -m pydoc raw_input/open/file/os/sys
---
+++ ex12.py
@@ -0,0 +1,7 @@
+﻿age = raw_input("How old are you?")  # raw_input(),括号里面可以直接写提示，加上双引号
+height = raw_input("How tall are you ?")
+weight = raw_input("How much do you weigh?")
+
+print "So, you're %r old, %r tall, %r heavy." %(
+   age, height, weight)
+#  %(age, height, weight) 直接这样转行下来不行
