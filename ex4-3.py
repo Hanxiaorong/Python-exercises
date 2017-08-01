0143ef73e262b1400a60a370e5e3a42b1c3723bb
Hanxiaorong <30439925+Hanxiaorong@users.noreply.github.com>
2017-08-01 11:14:21 +0800

    习题4

---
+++ ex4-3.py
@@ -0,0 +1,15 @@
+﻿i = 100
+# i = cad i = cad = 100 这样把i定义成字母不行，只有定义成数字，才能正常运行，不知道为什么？
+x = 12
+j = 10
+k = 25.0
+l = 5
+
+
+print i/x
+print i + x + j + k + l
+print x*j*l
+print i/k
+print i+x%j-k*l
+print k/l
+print l%k+j-i
