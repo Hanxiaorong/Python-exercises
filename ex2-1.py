19df4d9e1252ff1f3e13f0d9762ddd0e93242376
Hanxiaorong <30439925+Hanxiaorong@users.noreply.github.com>
2017-08-01 11:13:10 +0800

    习题2

---
+++ ex2-1.py
@@ -0,0 +1,8 @@
+﻿# print "韩小荣" #号是注释的作用，另外，之前一直不能输入中文，
+#有中文就报错现在换成UTF-8格式编码就可以了，还有，写完记得随时点击保存
+#    print "I like this coding."  空格的大小不影响代码的运行
+print   "This is so cool." #空格是为了代码美观。
+
+print  "she is a so beautiful girl.# Do you think?" # 注意区分#号所处的位置
+
+# 问题：不同编码格式有什么不同？
