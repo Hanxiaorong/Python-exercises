ea8038af454269c9938c16bb9e33b00efa5c42f7
Hanxiaorong <30439925+Hanxiaorong@users.noreply.github.com>
2017-08-01 11:19:51 +0800

    习题15

---
+++ ex15-1.py
@@ -0,0 +1,7 @@
+﻿print "Type the filename:" # 下面open要打开的具体文件名，print 要顶格写
+filename = raw_input('>')
+
+txt = open(filename) # 返回“file object”
+print txt.read() # 读取具体文件的内容
+# print txt.readline() 读取文件第一行
+# print txt.readlines() 读取文件的所有行
---
+++ ex15.py
@@ -0,0 +1,15 @@
+﻿from sys import argv # 从sys 中导入 agrv 这个模块
+
+script, filename = argv # 设置参数
+
+txt = open (filename) # 返回“ file object”
+
+print "Here's your file %r:" % filename # 返回具体的文件名
+print txt.read() # 打印具体文件名的内容
+
+print "Type the filename again:" # 再次打印具体文件名
+file_again = raw_input (">") # 输入文件名,这一行删除也能得到同样的结果
+
+txt_again = open(filename) # 返回“ file object”
+
+print txt_again.read() # 打印具体的文件内容
