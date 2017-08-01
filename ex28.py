4739b95f3ebcb8f51fde876bc9629fb57934ddb3
Hanxiaorong <30439925+Hanxiaorong@users.noreply.github.com>
2017-08-01 11:25:58 +0800

    习题28

---
+++ ex28.py
@@ -0,0 +1,42 @@
+True and True
+False and False
+1==1 and 2==1
+"Test" == "Test"
+1==1 or 2!=1
+True and 1==1
+False and 0!=0
+True or 1==1
+"testing" == "test"
+1!=0 and 2==1
+"test" != "testing"
+"test" == 1
+not (True and False)
+not (1==1 and 0!=1)
+not (10==1 or 1000==1000)
+not (1!=10 or 3==4)
+not ("testing" == "testing" and "Zed" == "Cool guy")
+1==1 and not ("testing" == 1 or 1==0)
+"chunky"  == "bacon" and (3==4 or 3==3)
+3==3 and not ("testing" == "testing" or "Python" == "Fun")
+
+
+# 1-True
+# 2-False
+# 3-False
+# 4-True
+# 5-True
+# 6-True
+# 7-False
+# 8-True
+# 9-False
+# 10-False
+# 11-True
+# 12-False
+# 13-True
+# 14-False
+# 15-False
+# 16-False
+# 17-True
+# 18-True
+# 19-False
+# 20-False
