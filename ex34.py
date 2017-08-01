05b6c3c97d0743f4b520636b4d9a3a71cc2cd1fa
Hanxiaorong <30439925+Hanxiaorong@users.noreply.github.com>
2017-08-01 11:29:23 +0800

    习题34

---
+++ ex34.py
@@ -0,0 +1,12 @@
+﻿animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+print animals[1] # 位置1的动物
+print animals[2] # 位置2的动物
+print animals[0] # 第一只动物
+print animals[3] # 第四只动物
+print animals[4] # 第五只动物
+print animals[3] # 位置3的动物
+print animals[5] # 位置5的动物，第六只动物
+print animals[4] # 第五只动物
+
+# 第1只动物在位置0，是一只熊；在位置0的是第1只动物，是一只熊。
