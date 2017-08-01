503dbf5ad353ca37e3d88201490e0246c09432a5
Hanxiaorong <30439925+Hanxiaorong@users.noreply.github.com>
2017-08-01 11:21:18 +0800

    习题18

---
+++ ex18-1.py
@@ -0,0 +1,6 @@
+﻿# this one is like your scripts with argv
+
+def ptintwo(arg1, arg2): # 新建一个函数，以def 开始，pintwo 是函数名，函数名可以任意换，但是不能以数字开头。紧跟括号，里面是参数，多个参数用逗号隔开，写完要写分号。
+    print "arg1 : %r, arg2: %r" %(arg1, arg2) # 打印函数
+		
+ptintwo("one","two") # 给函数参数赋值
---
+++ ex18-2.py
@@ -0,0 +1,24 @@
+﻿# this one is like your scripts with argv
+def print_two(*args):
+    arg1, arg2 = args
+    print "arg1: %r, arg2: %r" %(arg1,arg2)  # 在ex18.py 中报错，是因为print 前面不是4个空格，一定要空格键敲入。
+	
+# ok, that *args is actually pointless, we can just do this
+def print_two_again(arg1, arg2):
+    print "arg1: %r, arg2: %r" %(arg1,arg2)
+	
+# this just takes one argument
+def print_one(arg1):
+    print "arg1: %r" % arg1
+	
+# this one takes no arguments
+def print_none():
+    print "I got nothin'."
+
+
+print_two("A1877","Shaw")  
+print_two_again("A1877","Shaw")
+print_one("First!")
+print_none()	
+
+# IndentationError: unexpected indent 错误是指tab和空格混用了
