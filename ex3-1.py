799a9cc6fdcede1f56e6097254ee49ef02d927ff
Hanxiaorong <30439925+Hanxiaorong@users.noreply.github.com>
2017-08-01 11:13:48 +0800

    习题3

---
+++ ex3-1.py
@@ -0,0 +1,25 @@
+﻿print "I will now count my chickens:" #输出引号内的内容
+
+print  "Hens",25+30/6  #引号里面的内容直接输出，算式直接计算
+print "Roosters",100-25*3%4  #同理，引号里面的内容直接输出，算式直接计算，后半部分25乘以3%乘以4
+
+print "Now I will count the eggs:"
+
+print 3+2+1-5+4%2-1/4+6  #结果是整数，应该是5.83.有待验证？
+
+print "Is it true that 3+2<5-7?"
+
+print 3+2<5-7 #逻辑算式，直接进行判断
+
+print "What is 3+2?",3+2
+print "What is 5-7?",5-7
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+print "Is it greater?",5>-2
+print "Is it greater or equal?",5>=-2
+print "Is it less or equal?",5<=-2
+
+# 运行前记得先把编码格式改下，然后点击保存，在运行，否则会报错
